e06eeb339e38e55a25ef896d4224a31bd7db753a
Alla <15230497577@163.com>
2025-12-06 15:09:40 +0800

    chore: add GameInput actions asset with Player/Move bindings

--- Assets/Project/Scripts/Player/MPSoulActor.cs
+++ Assets/Project/Scripts/Player/MPSoulActor.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 // MPSoulActor represents the local player actor with basic health and hooks for controls.
 // Movement/rotation and combat are stubbed for now; focus on lifecycle and health API.
@@ -13,6 +14,8 @@ public class MPSoulActor : MonoBehaviour
 
     #region Fields
     private bool _isDead;
+    private GameInput _input;
+    private Vector2 _moveInput;
     #endregion
 
     #region Unity Lifecycle
@@ -24,6 +27,19 @@ public class MPSoulActor : MonoBehaviour
         }
 
         CurrentHp = MaxHp;
+
+        _input = new GameInput();
+        _input.Player.SetCallbacks(new PlayerActions(this));
+    }
+
+    private void OnEnable()
+    {
+        _input?.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _input?.Disable();
     }
 
     private void Update()
@@ -39,6 +55,11 @@ public class MPSoulActor : MonoBehaviour
         }
 
         HandleMovement();
+    }
+
+    private void OnDestroy()
+    {
+        _input?.Dispose();
     }
     #endregion
 
@@ -61,10 +82,7 @@ public class MPSoulActor : MonoBehaviour
     #region Private Methods
     private void HandleMovement()
     {
-        var inputX = Input.GetAxis("Horizontal");
-        var inputZ = Input.GetAxis("Vertical");
-
-        var inputDir = new Vector3(inputX, 0f, inputZ);
+        var inputDir = new Vector3(_moveInput.x, 0f, _moveInput.y);
 
         if (inputDir.sqrMagnitude > 0.001f)
         {
@@ -90,6 +108,28 @@ public class MPSoulActor : MonoBehaviour
         }
 
         gameObject.SetActive(false);
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInput = context.ReadValue<Vector2>();
+    }
+    #endregion
+
+    #region Input Wrapper
+    private class PlayerActions : GameInput.IPlayerActions
+    {
+        private readonly MPSoulActor _owner;
+
+        public PlayerActions(MPSoulActor owner)
+        {
+            _owner = owner;
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            _owner.OnMove(context);
+        }
     }
     #endregion
 }
